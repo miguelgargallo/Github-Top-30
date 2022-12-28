d2b5adc9e7f0e408bdb7ef774b3212b9c8518277
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-28 01:25:50 +0100

    Source Code 1.0.0

---
+++ play.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+chmod +x top100.py
+chmod +x play.sh
+
+pip install requests
+
+python ./top100.py
---
+++ top100.py
@@ -0,0 +1,29 @@
+import requests
+
+# Set the URL for the GitHub API endpoint
+url = "https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc"
+
+# Send a request to the GitHub API and store the response
+response = requests.get(url)
+
+# Check the status code to make sure the request was successful
+if response.status_code != 200:
+    print("Error: Could not retrieve data from GitHub")
+    exit(1)
+
+# Parse the JSON data from the response
+data = response.json()
+
+# Open the top100.md file for writing
+with open("top100.md", "w") as f:
+    # Write the heading for the top 100 list
+    f.write("# Top 100 Most Starred Projects on GitHub\n\n")
+
+    # Iterate through the first 100 items in the data
+    for i in range(100):
+        # Get the current repository information
+        repo = data["items"][i]
+        # Write the repository name and number of stars to the file
+        f.write(f"{i+1}. [{repo['name']}]({repo['html_url']}) - {repo['stargazers_count']} stars\n")
+
+print("Top 100 most starred projects written to top100.md")
